36a0399cc7e78a3969306c79780434b58565f68f
camilo hurtado <ch385593@gmail.com>
2024-09-23 12:07:23 -0500

    Archivos de trbajo lilián y contanza

---
+++ contanza parte b/js/validacion.js
@@ -0,0 +1,8 @@
+
+const userNameField=document.querySelector("[name=username");
+const passwordField=document.querySelector("[name=password]");
+const emailField=document.querySelector("[name=email]");
+
+userNameField.addEventListener("blur", function (){
+
+});
